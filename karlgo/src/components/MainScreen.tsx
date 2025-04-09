a7c09a712b98dd2c40958c3c14f8f3a151339fa8
Aleksandr <134843809+fireek1@users.noreply.github.com>
2025-04-09 12:33:53 +0200

    some changes

--- karlgo/src/components/MainScreen.tsx
+++ karlgo/src/components/MainScreen.tsx
@@ -3,6 +3,7 @@ import Maps from './Map';
 import SearchPanel from './SearchPanel';
 import './MainScreen.css';
 import OrangeLogo from '../assets/OrangeLogo2';
+import anal from '../assets/main-block.png';
 
 type MainScreenProps = {
   onAddBusinessClick: () => void;
@@ -34,10 +35,10 @@ const MainScreen: React.FC<MainScreenProps> = ({ onAddBusinessClick }) => {
     if (touchStartX.current !== null && touchEndX.current !== null) {
       const deltaX = touchEndX.current - touchStartX.current;
       if (Math.abs(deltaX) > 50) {
-        if (deltaX > 0 && currentScreen === 1) {
-          setCurrentScreen(0);
-        } else if (deltaX < 0 && currentScreen === 0) {
-          setCurrentScreen(1);
+        if (deltaX > 0 && currentScreen > 0) {
+          setCurrentScreen((prev) => prev - 1);
+        } else if (deltaX < 0 && currentScreen < 2) {
+          setCurrentScreen((prev) => prev + 1);
         }
       }
     }
@@ -50,8 +51,8 @@ const MainScreen: React.FC<MainScreenProps> = ({ onAddBusinessClick }) => {
       if (containerRef.current) {
         const gap = 64;
         const width = containerRef.current.offsetWidth;
-        const totalGap = gap * (2 - 1);
-        const slideW = (width - totalGap) / 2;
+        const totalGap = gap * (3 - 1); // 3 экрана
+        const slideW = (width - totalGap) / 3;
         setSlideWidth(slideW + gap);
       }
     };
@@ -69,7 +70,7 @@ const MainScreen: React.FC<MainScreenProps> = ({ onAddBusinessClick }) => {
     >
       <div className="top-bar">
         <OrangeLogo />
-        <button className='reg-new' onClick={onAddBusinessClick}>
+        <button className="reg-new" onClick={onAddBusinessClick}>
           Добавить бизнес
         </button>
       </div>
@@ -91,17 +92,21 @@ const MainScreen: React.FC<MainScreenProps> = ({ onAddBusinessClick }) => {
             <SearchPanel />
           </div>
         </div>
+        <div className="screen-slide">
+          <div className="inner-screen screen-anal">
+          <img src={anal} style={{height: '100%'}}/>
+          </div>
+        </div>
       </div>
 
       <div className="screen-indicator">
-        <div
-          className={`dot ${currentScreen === 0 ? 'active' : ''}`}
-          onClick={() => setCurrentScreen(0)}
-        />
-        <div
-          className={`dot ${currentScreen === 1 ? 'active' : ''}`}
-          onClick={() => setCurrentScreen(1)}
-        />
+        {[0, 1, 2].map((i) => (
+          <div
+            key={i}
+            className={`dot ${currentScreen === i ? 'active' : ''}`}
+            onClick={() => setCurrentScreen(i)}
+          />
+        ))}
       </div>
     </div>
   );
